68f1be2c48c67fe6c1a5119fea2d59e767f7994d
matthew-kapp <mattkapp.python@gmail.com>
2025-02-20 21:15:49 +0200

    Hydraulic update works

--- examples/hydraulic.jl
+++ examples/hydraulic.jl
@@ -4,29 +4,24 @@ using ModelingToolkitDesigner
 import ModelingToolkitStandardLibrary.Hydraulic.IsothermalCompressible as IC
 import ModelingToolkitStandardLibrary.Blocks as B
 
+@mtkmodel HydraulicSystem begin
 
-@component function System(; name)
-
-    pars = []
-
-    systems = @named begin
+    @components begin
         fluid = IC.HydraulicFluid(; density = 876, bulk_modulus = 1.2e9, viscosity = 0.034)
 
         stp = B.Step(; height = 10e5, start_time = 0.005)
         src = IC.Pressure()
         vol = IC.FixedVolume(; vol = 10.0)
-        res = IC.Tube(5; area = 0.01, length = 500.0)
+        res = IC.Tube(; N = 5, area = 0.01, length = 500.0)
     end
 
-    eqs = [
+    @equations begin
         connect(stp.output, src.p)
         connect(src.port, res.port_a)
         connect(vol.port, res.port_b)
         connect(src.port, fluid)
-    ]
+    end
 
-
-    ODESystem(eqs, t, [], pars; name, systems)
 end
 
 @named sys = System()
@@ -35,7 +30,7 @@ path = joinpath(@__DIR__, "design") # folder where visualization info is saved a
 design = ODESystemDesign(sys, path);
 ModelingToolkitDesigner.view(design)
 
-# using CairoMakie
-# CairoMakie.set_theme!(Theme(;fontsize=12))
-# fig = ModelingToolkitDesigner.view(design, false)
-# save(joinpath(@__DIR__, "hydraulic.svg"), fig; resolution=(400,200))
+using CairoMakie
+CairoMakie.set_theme!(Theme(;fontsize=12))
+fig = ModelingToolkitDesigner.view(design, false)
+save(joinpath(@__DIR__, "hydraulic.svg"), fig; size=(400,200))
